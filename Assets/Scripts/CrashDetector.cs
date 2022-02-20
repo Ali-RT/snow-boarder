bb4ce92bfde9e25e52653c43bf74d98a811bfa42
Ali Rahim Taleqani <36344134+Ali-RT@users.noreply.github.com>
2022-02-20 16:54:04 -0600

    add particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
-    void OnTriggerEnter2D(Collider2D other)
+	[SerializeField] ParticleSystem crashEffect;
+	void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
+			crashEffect.Play();
 			Invoke("ReloadScene", loadDelay);
 		}
 	}
